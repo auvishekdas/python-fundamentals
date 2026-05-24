3f43fc8e04dd9e864ebc2f13dd830abc64fb8c35
Auvishek Das <auvishekdas9@gmail.com>
2026-05-24 10:08:02 -0700

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,19 @@
+# my biodata program
+print("Auvishek Das")
+print("01813009642")
+
+'''
+This
+is
+a
+multiple line
+comment
+'''
+print("Auvishek Das")
+print("01813009642")
+
+print("Auvishek Das 01813009642")
+print("Auvishek Das \n 01813009642")
+print("Auvishek Das \t 01813009642")
+print("\"Auvishek Das\"")
+print("\'Auvishek Das\'")

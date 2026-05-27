be68cf575611635c58a3cf8c4d6e8540bb70f80d
Auvishek Das <auvishekdas9@gmail.com>
2026-05-27 06:21:25 -0700

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,12 @@
+
+name = input("Enter your name : ")
+age = input("Enter your age : ") 
+gpa = input("Enter your gpa : ")
+
+
+
+print("student Information")
+print("--------------------------")
+print("Name : "+name)
+print("Age  : "+age )
+print("Gpa  : "+gpa )

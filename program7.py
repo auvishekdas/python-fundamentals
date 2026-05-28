92dcbdedeef714e674f226f898783fb5b91ea02b
Auvishek Das <auvishekdas9@gmail.com>
2026-05-28 02:34:53 -0700

    Add files via upload

---
+++ program7.py
@@ -0,0 +1,17 @@
+
+base = float (input("Enter base = "))
+height = float (input("Enter height = "))
+
+area = 0.5 * base * height
+print("Area of triangle = ",area)
+
+width = float (input("Enter width = "))
+height = float (input("Enter height = "))
+
+area = width * height
+print("Area of rectangle = ",area)
+
+radius = float(input("Enter radius = "))
+
+area = 3.1416 * radius * radius
+print("Area of circle = ",area)

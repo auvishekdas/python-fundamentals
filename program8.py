7c31d3db0ebfbfc95bf2f39be3603c5869a79926
Auvishek Das <auvishekdas9@gmail.com>
2026-05-29 08:41:42 -0700

    Add files via upload

---
+++ program8.py
@@ -0,0 +1,9 @@
+from math import *
+
+print(max(40,50))
+print(min(30,40))
+print(abs(-10))
+print(sqrt(81))
+print(round(9.5))
+print(floor(8.8))
+print(ceil(8.8))
---
+++ program9.py
@@ -0,0 +1,21 @@
+num = 45
+print(type(num))
+
+num = 30.5
+print(type(num))
+
+num = "Auvishek"
+print(type(num))
+
+print(type(False))
+
+num1= 25
+num2= 35
+print("Sum is ",num1+num2)
+
+num1 = 18
+num2 = 14
+print(f"{num1} - {num2} = {num1-num2} ")
+
+print("Auvishek Das.",end=" " )
+print("01813009642")

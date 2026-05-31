11fc286168e6da21f6cf9b0a3f8645c83288e967
Auvishek Das <auvishekdas9@gmail.com>
2026-05-31 14:58:36 +0600

    Add files via upload

---
+++ program11.py
@@ -0,0 +1,29 @@
+#pass/fail
+mark = 75
+if mark >= 33:
+   print("Pass")
+else :
+    print("Fail")
+    
+#pass/fail
+mark = 25
+if mark >= 33:
+    print("Pass")
+else:
+    print("Fail")
+    
+#largest
+num1 = 60
+num2 = 30
+if num1 > num2:
+    print("num1")
+else:
+    print("num2")
+    
+#even/odd
+num = -2
+if num%2 == 0:
+    print("Even")
+else:
+    print("Odd")
+  

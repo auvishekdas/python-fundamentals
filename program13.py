0980ae8ad9f3e39de370653955c5a88a61cdafde
Auvishek Das <auvishekdas9@gmail.com>
2026-06-02 16:13:57 +0600

    Add files via upload

---
+++ program13.py
@@ -0,0 +1,26 @@
+if 9>5:
+    if 12>3:
+        print("Hi")
+    else:
+        print("Hello")
+if 9>8:
+    if 12>35:
+        print("Hi")
+    else:
+        print("Hello")
+        
+num1 = 30
+num2 = 10
+num3 = 40
+if num1 > num2:
+    if num1 > num3:
+        print(num1)
+    else:
+        print(num3)
+        
+if num2 > num1:
+    if num2 > num3:
+       print(num2)
+    else:
+        print(num3)
+    

59c672b30bef01abf883000293609a427e95f44d
Auvishek Das <auvishekdas9@gmail.com>
2026-06-03 16:31:51 +0600

    Add files via upload

---
+++ program14.py
@@ -0,0 +1,11 @@
+num1 = 40
+num2 = 60
+"""
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)
+    
+"""
+max = num1 if num1 > num2 else num2
+print("Maximum = ",max)
---
+++ program15.py
@@ -0,0 +1,16 @@
+num1 = 20
+num2 = 50
+num3 = 60
+if num1 > num2 and num1 > num3:
+    print(num1)
+elif num2 > num1 and num2 > num3:
+    print(num2)
+else:
+    print(num3)
+    
+#vowel - a,e,i,o,u
+v = 'w'
+if v == 'a' or v == 'e' or v == 'i' or v == 'o' or v == 'u':
+    print("vowel")
+else:
+    print("consonant")

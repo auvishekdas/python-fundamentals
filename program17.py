9fee5fe2ccbba88af295e2b7f9cdb40e71800c77
Auvishek Das <auvishekdas9@gmail.com>
2026-06-05 16:59:07 +0600

    Add files via upload

---
+++ program17.py
@@ -0,0 +1,25 @@
+
+i = 1
+while i <= 15:
+    print(i)
+    i = i + 1
+print("Program end")
+
+i = 1
+while i <= 5:
+    print("Auvishek")
+    i = i + 1
+print("Program end")
+
+i = 2
+while i <= 20:
+    print(i)
+    i = i +2
+print("Program end")
+
+i = 1
+while i <= 20:
+    print(i)
+    i = i + 2
+print("Program end")    
+    

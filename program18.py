c74c28c0ca37e6a3a56a3db0e0e0b04c9c3ff2f9
Auvishek Das <auvishekdas9@gmail.com>
2026-06-06 22:56:27 +0600

    Add files via upload

---
+++ program18.py
@@ -0,0 +1,22 @@
+
+n = int (input("Enter the last term : "))
+sum = 0
+i = 1
+while i <= n:
+    sum = sum + i
+    i = i + 1
+    
+print (sum)
+
+
+#3 + 6 +9 ....... + n
+n = int (input("Enter the last term : "))
+sum = 0
+i = 3
+while i <= n:
+    sum = sum + i
+    i = i + 3
+    
+print(sum)    
+    
+    

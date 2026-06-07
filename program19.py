7e22ce105a092879a3d610a500f75c721d2f734b
Auvishek Das <auvishekdas9@gmail.com>
2026-06-07 18:03:05 +0600

    Add files via upload

---
+++ program19.py
@@ -0,0 +1,24 @@
+
+
+i = 1
+while i <= 50:
+    if i == 25:
+        break
+    print(i)
+    i = i + 1
+print("End")
+
+
+i = 1
+while i <= 30:
+    if i == 28:
+        continue
+    print(i)
+    i = i + 1
+print("End")    
+    
+
+
+
+
+
---
+++ program20.py
@@ -0,0 +1,12 @@
+
+subjects = ["English","German","Programming","Git"]
+print(subjects)
+print(subjects[0])
+print(subjects[3])
+print(subjects[1:])
+print(subjects[-2])
+print("German" in subjects)
+print("C++" in subjects)
+print("C" not in subjects)
+print(subjects + ["Practice"])
+print(subjects * 2)

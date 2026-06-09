ab9de450da0d79f2350993990b424b0e6d06606a
Auvishek Das <auvishekdas9@gmail.com>
2026-06-09 19:53:56 +0600

    Add files via upload

---
+++ program22.py
@@ -0,0 +1,14 @@
+num = list(range(15))
+print(num)
+
+num = list(range(12))
+print(num[5])
+
+num = list(range(10,20))
+print(num)
+
+num = list(range(22,44,2))
+print(num)
+
+num = list(range(11,61,5))
+print(num)

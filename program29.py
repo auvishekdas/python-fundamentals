0d3b15361b97198ae50f8a258adc564122512dde
Auvishek Das <auvishekdas9@gmail.com>
2026-06-13 21:43:00 +0600

    Add files via upload

---
+++ program29.py
@@ -0,0 +1,29 @@
+studentId = {
+    
+    "11" : "Anik",
+    "12" : "Rahim",
+    "13" : " Emon",
+    "14" : "Hasan",
+    "15" : "Zaman",
+}
+print(studentId["12"])
+
+studentId = {
+    
+    "11" : "Anik",
+    "12" : "Rahim",
+    "13" : " Emon",
+    "14" : "Hasan",
+    "15" : "Zaman",
+}
+print(studentId.get("14"))
+
+studentId = {
+    
+    "11" : "Anik",
+    "12" : "Rahim",
+    "13" : " Emon",
+    "14" : "Hasan",
+    "15" : "Zaman",
+}
+print(studentId.get("18","Not a valid key"))

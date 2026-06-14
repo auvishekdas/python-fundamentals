a74690ce9cbae5287d1b86241303c60164ff3775
Auvishek Das <auvishekdas9@gmail.com>
2026-06-14 17:06:25 +0600

    Add files via upload

---
+++ program30.py
@@ -0,0 +1,40 @@
+students  = (
+    
+    "Fahim Uddin",
+    "Liton Sheikh",
+    "Nasir Mahmud",
+    "Billal Sheikh",
+    "Farid Uddin",
+)
+
+print(students[3])
+
+students  = (
+    
+    "Fahim Uddin",
+    "Liton Sheikh",
+    "Nasir Mahmud",
+    "Billal Sheikh",
+    "Farid Uddin",
+)
+print(students)
+
+students  = (
+    
+    "Fahim Uddin",
+    ("Liton Sheikh",30,4.9),
+    "Nasir Mahmud",
+    "Billal Sheikh",
+    "Farid Uddin",
+)
+print(students[1])
+
+students  = (
+    
+    "Fahim Uddin",
+    ("Liton Sheikh",30,4.9),
+    "Nasir Mahmud",
+    "Billal Sheikh",
+    "Farid Uddin",
+)
+print(students[2:]) 

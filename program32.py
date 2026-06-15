4a783da85d8cf4f6057bb0ef337dbdf78c2d5630
Auvishek Das <auvishekdas9@gmail.com>
2026-06-15 17:25:49 +0600

    Add files via upload

---
+++ program32.py
@@ -0,0 +1,25 @@
+Languages = []
+Languages.append("English")
+Languages.append("German")
+Languages.append("Hindi")
+Languages.append("Bangla")
+Languages.append("Arabic")
+print(Languages)
+
+Languages.pop ()
+print("Now the top language is : ",Languages[-1])
+
+Languages.pop ()
+print("Now the top language is : ",Languages[-1])
+
+Languages.pop ()
+print("Now the top language is : ",Languages[-1])
+
+Languages.pop ()
+print("Now the top language is : ",Languages[-1])
+
+Languages.pop ()
+if not Languages:
+    print("No languages left")
+
+

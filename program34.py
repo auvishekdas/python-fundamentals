052a4ecf2d1ee059dc1ab788dbeff980ee12caaf
Auvishek Das <auvishekdas9@gmail.com>
2026-06-16 17:07:21 +0600

    Add files via upload

---
+++ program34.py
@@ -0,0 +1,23 @@
+def sum(a,b,c):
+    result = a + b + c
+    print(result)
+sum(20,25,32)
+
+def subtract(x,y):
+    result = x - y
+    print(result)
+subtract(30,25)
+
+def product(l,m,n):
+    result = l * m * n
+    print(result)
+product(25,25,48)
+
+def quotient(j,k):
+    result = j / k 
+    print(result)
+quotient(25,5)
+
+def text():
+    print("No text")
+text()    

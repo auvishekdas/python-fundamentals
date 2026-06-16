619ee2204ce4570c27166beb46d9bea1cfee4d69
Auvishek Das <auvishekdas9@gmail.com>
2026-06-16 17:07:41 +0600

    Add files via upload

---
+++ program35.py
@@ -0,0 +1,14 @@
+def sum(a,b,c):
+    total = a + b + c
+    return total
+result = sum(25,30,35)
+print("Result = ",result)
+
+def large(x,y):
+    if x > y:
+        return x
+    else:
+        return y
+maximum = large
+print(maximum(55,40))
+

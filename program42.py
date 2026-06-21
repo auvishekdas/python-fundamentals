fc573d9cc6bde8e985475f94d7a5327396b55f5e
Auvishek Das <auvishekdas9@gmail.com>
2026-06-21 21:39:29 +0600

    Add files via upload

---
+++ program42.py
@@ -0,0 +1,7 @@
+serial = [1,2,3,4,5]
+language = ["Bangla","English","Hindi","German","Arabic"]
+print (list(zip(serial,language)))
+
+serial = [1,2,3,4,5]
+language = ["Bangla","English","Hindi","German","Arabic"]
+print (list(zip(serial,language,"ABCDE")))

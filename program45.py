3ccdc368f287e1faa1616d75e8f4055bee1ad31a
Auvishek Das <auvishekdas9@gmail.com>
2026-06-24 21:32:49 +0600

    Add files via upload

---
+++ program45.py
@@ -0,0 +1,8 @@
+file = open("information1.txt","a")
+file.write("\nLanguage: English & German")
+file.close()
+
+
+file = open("information2.txt","w")
+file.write("\nLanguage: English & German")
+file.close()

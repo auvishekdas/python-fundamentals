e5e5fca88af56729de1c684e8157dd2af4884a39
Auvishek Das <auvishekdas9@gmail.com>
2026-06-28 15:48:44 +0600

    Add files via upload

---
+++ program50.py
@@ -0,0 +1,12 @@
+class People:
+    Serial = ""
+    Age = ""
+Hasan = People()
+Hasan.Serial = 11
+Hasan.Age = 20
+print(f"Serial : {Hasan.Serial}, Age : {Hasan.Age}")
+
+Habib = People()
+Habib.Serial = 12
+Habib.Age = 25
+print(f"Serial : {Habib.Serial}, Age : {Habib.Age}")

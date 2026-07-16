6eef8c18b3f226aab9758b735633b90eae2d2e22
Auvishek Das <auvishekdas9@gmail.com>
2026-07-16 17:58:15 +0600

    Add files via upload

---
+++ program67_coding_standards.py
@@ -0,0 +1,22 @@
+#Naming Consistency Practice
+
+user_name = "Habib"
+user_age = 25
+user_gpa = 5.00
+
+print(f"User Name : {user_name}")
+print(f"User Age : {user_age}")
+print(f"User GPA : {user_gpa}")
+
+#Avoid Over-commeting Practice
+
+#Price calculation
+original_price = 100
+total_price = original_price + 15
+print("Total price is : ",total_price)
+
+#Whitespace Practice
+
+print("Step 1")
+
+print("Step 2")
